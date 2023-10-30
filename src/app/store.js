cd780236744cbdaefcdd58eb1cd2e8d2d5c3c100
Daniel Charman <hello@danielcharman.com>
2023-10-30 20:23:47 +1100

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
-import planetReducer from '../features/planets/planetSlice';
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
-		planets: planetReducer,
 	},
 });
